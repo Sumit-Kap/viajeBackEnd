f4ebb7e1fd2a82c60f03e57edcf62d96c3931c6f
Sumit-Kap <smtkpr88@gmail.com>
2019-01-28 23:44:00 +0530

    first commit

---
+++ viaje/src/main/java/com/viaje/services/viaje/ViajeApplication.java
@@ -0,0 +1,14 @@
+package com.viaje.services.viaje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViajeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViajeApplication.class, args);
+	}
+
+}
+
---
+++ viaje/src/test/java/com/viaje/services/viaje/ViajeApplicationTests.java
@@ -0,0 +1,17 @@
+package com.viaje.services.viaje;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ViajeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
